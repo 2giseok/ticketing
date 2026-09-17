120a7ef6016b75571692939d22db124fc44671c6
2giseok <rltjr147@gmail.com>
2026-09-17 17:34:06 +0900

    step1 검증

--- src/main/java/com/project/ticketing_concurrency_lab/domain/IssuedTicket.java
+++ src/main/java/com/project/ticketing_concurrency_lab/domain/IssuedTicket.java
@@ -3,6 +3,7 @@ package com.project.ticketing_concurrency_lab.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class IssuedTicket {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
     private Ticket ticket;
